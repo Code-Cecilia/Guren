95b67e8b2d0369f194f0f9d297ede0f03c24e947
Yuichiro <ruicabral11@hotmail.com>
2020-07-09 17:57:55 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,27 +1,15 @@
-# This file will contain all of our website's routes
 
 from dashcord import route
-# Route method formating is shown below:
 
 @route("/")
 async def index(bot, request):
     """The url path is defined within the @route decorator"""
-    # request.method returns either GET or POST
     if request.method == "GET":
-        # We have a GET request.
-        
-        # As this is an example of a guild counter, we want to pass the bot guilds
-        # to the template formatter.
         guilds = len(bot.guilds)
         
         return bot.dashboard.render_html("index.html", guilds=guilds)
-        # bot.dashboard.render_html will format the html file for you.
     
     else:
-        # We have a post request.
-        # If the post request is a form, the request json will return the data within
-        # the form.
-        
         json = await request.json()
         
         channel = bot.get_channel()
