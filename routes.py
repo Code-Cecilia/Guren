221eb051bd27ee5ef237c1af34b72b9713616196
Yuichiro <ruicabral11@hotmail.com>
2020-07-10 00:04:28 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,18 +1,22 @@
 
+import discord
+from discord.ext import commands
 from dashcord import route
+
+
 
 @route("/")
 async def index(bot, request):
     """The url path is defined within the @route decorator"""
+
     if request.method == "GET":
         guilds = len(bot.guilds)
         
         return bot.dashboard.render_html("index.html", guilds=guilds)
-    
     else:
         json = await request.json()
         
         channel = bot.get_channel()
-        await channel.send("We just made a POST request with the data: `{}`".format(str(json)))
+        await channel.send("POST Request received with the data: {}".format(str(json)))
         
         return bot.dashboard.render_html("index.html", guilds=guilds)
