40d7f035f247d9e6d95649f2e71c6a8d6d7236bf
Yuichiro <ruicabral11@hotmail.com>
2020-07-10 14:21:33 +0100

    Update startpy.sh

--- startpy.sh
+++ startpy.sh
@@ -1,2 +1,9 @@
 !#/bin/sh
+REQUIRED_PKG="python3"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "$REQUIRED_PKG is not installed. Installing $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+fi
 python3 botstart.py
