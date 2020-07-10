6b4773db9b698f1c168dd1c76073c2082d1313eb
Yuichiro <ruicabral11@hotmail.com>
2020-07-10 14:18:09 +0100

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -31,8 +31,9 @@ class Events(commands.Cog):
             await ctx.send("Hey! You lack permission to use this command.")     
         if isinstance(error, commands.CommandNotFound):
             await ctx.send("Command not found!")
-        else:
+        else:    
             raise error
+
 
 def setup(bot):
     bot.add_cog(Events(bot))
