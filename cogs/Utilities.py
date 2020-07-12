de054ad2fe31b890942932d0aef99daef0df7c11
Yuichiro <ruicabral11@hotmail.com>
2020-07-12 16:57:19 +0100

    Update Utilities.py

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -7,6 +7,10 @@ class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+        
     @commands.command(name="avatar")
     @commands.guild_only()
     async def avatar(self, ctx, member: discord.Member = None):
