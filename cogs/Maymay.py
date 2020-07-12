e36a52174326a82e61aee7efa2f23660c7ff2bc2
Yuichiro <ruicabral11@hotmail.com>
2020-07-12 16:55:46 +0100

    Update Maymay.py

--- cogs/Maymay.py
+++ cogs/Maymay.py
@@ -17,6 +17,10 @@ class Maymay(commands.Cog):
     def __init__ (self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+ 
     @commands.command()
     async def meme(self, ctx, member: discord.Member = None):
         """Sends a random meme"""
