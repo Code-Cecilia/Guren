7c520a085e23028d751d9dcbd979d19b413b489b
BuildTools <unconfigured@null.spigotmc.org>
2021-07-03 23:05:55 +0000

    teste

--- botstart.py
+++ botstart.py
@@ -5,16 +5,22 @@ import json
 import os
 import random
 import logging
-
+import contextlib
+import sqlite3
 
 import discord
 from discord.ext import commands
 from pathlib import Path
 import motor.motor_asyncio
+import io
+import textwrap
+import traceback
+from traceback import format_exception
 
 import utils.json_loader
 from utils.mongo import Document
-
+from utils.util import clean_code, Pag
+from discord_slash import SlashCommand
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
@@ -23,9 +29,12 @@ print(f"{cwd}\n-----")
 
 description = '''A clever discord bot written in python for the guild Uploading Nation'''
 
+initial_extensions = ['cogs.leveling']
+
+
 async def get_prefix(bot, message):
     if not message.guild:
-        return commands.when_mentioned_or("gb$")(bot, message)
+        return commands.when_mentioned_or("g$")(bot, message)
 
     try:
         data = await bot.config.find(message.guild.id)
@@ -38,14 +47,15 @@ async def get_prefix(bot, message):
 
 
 secret_file = utils.json_loader.read_json('secrets')
-
-
+intents = discord.Intents.all()
 bot = commands.Bot(
     command_prefix=get_prefix, 
     description=description,
     owner_id=219410026631135232,
-    case_insensitive=True
+    case_insensitive=True,
+    intents=intents
 )
+slash = SlashCommand(bot, sync_commands=True)
 
 bot.config_token = secret_file["token"]
 logging.basicConfig(level=logging.INFO)
@@ -85,9 +95,12 @@ async def on_ready():
     print('Bot latency:', bot.latency*1000, 2)
     print('Running discord.py version ' + discord.__version__)
     bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(str(bot.connection_url))
-    bot.db = bot.mongo["menudocs"]
+    bot.db = bot.mongo["Guren"]
     bot.config = Document(bot.db, "config")
+    bot.warns = Document(bot.db, "warns")
     bot.mutes = Document(bot.db, "mutes")
+    bot.command_usage = Document(bot.db, "command_usage")
+    bot.reaction_roles = Document(bot.db, "reaction_roles")
 
     print("Initialized Database\n-----")
     for document in await bot.config.get_all():
@@ -98,6 +111,65 @@ async def on_ready():
         bot.muted_users[mute["_id"]] = mute
 
     print(bot.muted_users)
+
+@bot.event
+async def on_guild_join(guild):
+    main = sqlite3.connect('Leveling/main.db')
+    cursor = main.cursor()
+    cursor.execute(f"SELECT enabled FROM glevel WHERE guild_id = '{guild.id}'")
+    result = cursor.fetchone()
+    if result is None:
+        sql = ("INSERT INTO glevel(guild_id, enabled) VALUES(?,?)")
+        val = (str(guild.id), 'enabled')
+        cursor.execute(sql, val)
+        main.commit()
+    elif str(result[0]) == 'disabled':
+        sql = ("UPDATE glevel SET enabled = ? WHERE guild_id = ?")
+        val = ('enabled', str(guild.id))
+        cursor.execute(sql, val)
+        main.commit()
+    cursor.close()
+    main.close()
+
+@bot.command(name="eval", aliases=["exec"])
+@commands.is_owner()
+async def _eval(ctx, *, code):
+    """Owner only command"""
+    code = clean_code(code)
+
+    local_variables = {
+        "discord": discord,
+        "commands": commands,
+        "bot": bot,
+        "ctx": ctx,
+        "channel": ctx.channel,
+        "author": ctx.author,
+        "guild": ctx.guild,
+        "message": ctx.message
+    }
+
+    stdout = io.StringIO()
+
+    try:
+        with contextlib.redirect_stdout(stdout):
+            exec(
+                f"async def func():\n{textwrap.indent(code, '    ')}", local_variables,
+            )
+
+            obj = await local_variables["func"]()
+            result = f"{stdout.getvalue()}\n-- {obj}\n"
+    except Exception as e:
+        result = "".join(format_exception(e, e, e.__traceback__))
+
+    pager = Pag(
+        timeout=100,
+        entries=[result[i: i + 2000] for i in range(0, len(result), 2000)],
+        length=1,
+        prefix="```py\n",
+        suffix="```"
+    )
+
+    await pager.start(ctx)
 
 @bot.event
 async def on_message(message):
