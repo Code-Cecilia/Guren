0f6f53b2acbd7df8b88f659097a637838e154374
BuildTools <unconfigured@null.spigotmc.org>
2021-07-07 16:56:05 +0000

    add logging

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -7,6 +7,10 @@ class Roleplay(commands.Cog):
     """Roleplay commands. If you'd like to add a gif to the bot database please contact me on discord: Yuichiro#0001"""
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
     
     @commands.command()
     async def cookie(self, ctx, member: discord.Member = None):
