af675e78b2823122371740dea366a7815fe569ec
BuildTools <unconfigured@null.spigotmc.org>
2021-07-07 16:55:39 +0000

    start devloping activities, beta feature

---
+++ cogs/activities.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from discordTogether import DiscordTogether
+
+class Activities(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.togetherControl = DiscordTogether(bot)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded\n-----")
+
+    @commands.command()
+    async def start(self, ctx):
+        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
+        await ctx.send(f"Click the blue link!\n{link}")
+
+def setup(client):
+    client.add_cog(Activities(client))
