c123330ef9c1214c28e01218ab1f340a7459651f
Yuichiro <yhyakuya@gmail.com>
2021-07-10 11:50:17 +0000

    add stuff

--- cogs/activities.py
+++ cogs/activities.py
@@ -10,7 +10,9 @@ class Activities(commands.Cog):
     async def on_ready(self):
         print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 
-    @commands.command()
+    @commands.command(
+        description=f"Currently Only supports youtube.\nStarts a youtube together activity, must be on a voice channel!\n**Usage:** g$start"
+        )
     async def start(self, ctx):
         link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
         await ctx.send(f"Click the blue link!\n{link}")
